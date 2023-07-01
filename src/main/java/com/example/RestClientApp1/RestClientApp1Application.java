30eaa02a0fa1a6fd87bb31f94f693fbc588d56b6
ashu7997 <ashvinisonawane82@gmail.com>
2023-07-01 18:28:38 +0530

    1st commited by me

---
+++ src/main/java/com/example/RestClientApp1/RestClientApp1Application.java
@@ -0,0 +1,13 @@
+package com.example.RestClientApp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestClientApp1Application.class, args);
+	}
+
+}
